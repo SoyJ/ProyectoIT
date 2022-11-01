22b869d587b639359fa57971c5852b6cdf7c0280
SoyJ <judyrojasvalencia@gmail.com>
2022-11-01 01:35:46 -0300

    Creando Templates

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -9,3 +9,18 @@ def curso(self):
     curso.save()
     texto= f"Curso creado : {curso.nombre} {curso.camada}"
     return HttpResponse (texto)
+
+def inicio(request):
+    return render (request, 'AppCoder/inicio.html')
+
+def cursos(request):
+    return render (request, 'AppCoder/cursos.html')
+
+def profesores(request):
+    return render (request, 'AppCoder/profesores.html')
+
+def estudiantes(request):
+    return render (request, 'AppCoder/estudiantes.html')
+
+def entregables(request):
+    return render (request, 'AppCoder/entregables.html')
